e977c42017dd6681da998dfa41f41fdf31bef8ca
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-07 17:57:20 +0200

    added requests

---
+++ Module5HW1/Models/Requests/AuthRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models.Requests;
+
+public class AuthRequest
+{
+    [JsonProperty("email")]
+    public string Email { get; set; } = default!;
+
+    [JsonProperty("password")]
+    public string Password { get; set; } = default!;
+}
