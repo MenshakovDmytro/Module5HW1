013390a84a9c78ff87f107cea787835e1ea73590
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-07 17:59:13 +0200

    added abstractions for services

---
+++ Module5HW1/Service/Abstraction/IResourceService.cs
@@ -0,0 +1,8 @@
+﻿namespace Module5HW1.Service.Abstraction;
+
+public interface IResourceService
+{
+    public Task GetListResource();
+    public Task GetSingleResource();
+    public Task GetSingleResourceNotFound();
+}
---
+++ Module5HW1/Service/Abstraction/IUserService.cs
@@ -0,0 +1,13 @@
+﻿namespace Module5HW1.Service.Abstraction;
+
+public interface IUserService
+{
+    public Task GetListOfUsers();
+    public Task GetSingleUser();
+    public Task GetSingleUserNotFound();
+    public Task CreateUser();
+    public Task UpdateUserPut();
+    public Task UpdateUserPatch();
+    public Task DeleteUser();
+    public Task GetDelayedResponse();
+}
