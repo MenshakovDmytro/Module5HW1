e04bfd0d3470588fe9719c3fa4468b1b7d0623de
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-07 17:58:01 +0200

    added Dtos

---
+++ Module5HW1/Models/ResourceDto.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models;
+
+public class ResourceDto
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; } = default!;
+
+    [JsonProperty("year")]
+    public int Year { get; set; }
+
+    [JsonProperty("color")]
+    public string Color { get; set; } = default!;
+
+    [JsonProperty("pantone_value")]
+    public string PantoneValue { get; set; } = default!;
+}
---
+++ Module5HW1/Models/SupportDto.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Module5HW1.Models;
+
+public class SupportDto
+{
+    [JsonProperty("url")]
+    public string Url { get; set; } = default!;
+
+    [JsonProperty("text")]
+    public string Text { get; set; } = default!;
+}
