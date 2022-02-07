a42a456dbe80ccb61e28c2d801644405ce0a05b2
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-07 17:58:30 +0200

    added ConfigProvider

---
+++ Module5HW1/Provider/Abstraction/IConfigProvider.cs
@@ -0,0 +1,8 @@
+﻿using Module5HW1.Models.Configs;
+
+namespace Module5HW1.Provider.Abstraction;
+
+public interface IConfigProvider
+{
+    public Config Config { get; }
+}
---
+++ Module5HW1/Provider/ConfigProvider.cs
@@ -0,0 +1,25 @@
+﻿using Module5HW1.Models.Configs;
+using Module5HW1.Provider.Abstraction;
+using Newtonsoft.Json;
+
+namespace Module5HW1.Provider;
+
+public class ConfigProvider : IConfigProvider
+{
+    private const string ConfigFile = "appconfig.json";
+
+    private readonly Config _config;
+
+    public ConfigProvider()
+    {
+        _config = LoadConfig().GetAwaiter().GetResult();
+    }
+
+    public Config Config => _config;
+
+    private async Task<Config> LoadConfig()
+    {
+        var configFile = await File.ReadAllTextAsync(ConfigFile);
+        return JsonConvert.DeserializeObject<Config>(configFile);
+    }
+}
